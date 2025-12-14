c87d94de29ebbddd82e65101de7898e1f98e907b
Steven <nsteven2004@gmail.com>
2025-12-14 12:35:00 +1100

    change refresh rate, fix import bug

--- gps/__init__.py
+++ gps/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 from .gps_reader import GPSReader
-from .gps_config import set_rate_5hz
+from .gps_config import set_rate_10hz
 
-__all__ = ["GPSReader", "set_rate_5hz"]
+__all__ = ["GPSReader", "set_rate_10hz"]
 
